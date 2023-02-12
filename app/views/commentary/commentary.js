60a3ff0bed62ca48ca638ca9abddb4efe72717fa
Michal Fulier <fuliermichal96@gmail.com>
2023-02-12 22:27:09 +0000

    get the chapter content

--- app/views/commentary/commentary.js
+++ app/views/commentary/commentary.js
@@ -12,6 +12,7 @@ export const Commentary = ({ navigation }) => {
   const [chaptersNumber, setChaptersNumber] = useState(null)
   const [currentData, setCurrentData] = useState({ book: 'Zjevení', chapter: 1 })
   const [chapterVisible, setChapterVisible] = useState(currentData.book)
+  const [chapterContent, setChapterContent] = useState({})
 
   useEffect(() => {
     const http = axios.create({
@@ -21,13 +22,14 @@ export const Commentary = ({ navigation }) => {
     http
       .get('/bibles/c0209b58481727a2-01/books').then(response => {
         setBibleData(response.data)
-        // console.log(response.data.data[0].id)
+      // console.log(response.data.data[0].id)
       })
       .catch(function (error) {
         // handle error
         alert(error.message)
       })
     getBookData()
+    getChapterContent()
   }, [])
 
   const getBookData = () => {
@@ -46,22 +48,42 @@ export const Commentary = ({ navigation }) => {
       })
   }
 
+  const getChapterContent = () => {
+    const http = axios.create({
+      baseURL: 'https://api.scripture.api.bible/v1',
+      headers: { 'api-key': API_KEY }
+    })
+    http
+      .get('/bibles/c0209b58481727a2-01/passages/REV.21').then(response => {
+        convertText(response.data.data.content)
+      })
+      .catch(function (error) {
+        // handle error
+        alert(error.message)
+      })
+  }
+
+  const convertText = (text) => {
+    console.log(text)
+    // setChapterContent(convertedText)
+  }
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerTitle: () => (
-                <TouchableOpacity
-                    activeOpacity={1}
-                    style={{
-                      backgroundColor: '#566370',
-                      padding: 7,
-                      borderRadius: 15,
-                      marginBottom: 10
-                    }}
-                    onPress={() => {
-                      setModalVisible(true)
-                    }}>
-                    <Text style={{ color: 'white', fontWeight: 'bold' }}>{currentData.book}</Text>
-                </TouchableOpacity>
+          <TouchableOpacity
+              activeOpacity={1}
+              style={{
+                backgroundColor: '#566370',
+                padding: 7,
+                borderRadius: 15,
+                marginBottom: 10
+              }}
+              onPress={() => {
+                setModalVisible(true)
+              }}>
+            <Text style={{ color: 'white', fontWeight: 'bold' }}>{currentData.book}</Text>
+          </TouchableOpacity>
       ),
       headerStyle: {
         backgroundColor: '#2D343B',
@@ -76,79 +98,79 @@ export const Commentary = ({ navigation }) => {
   }
 
   const Item = ({ bookName }) => (
-        <View style={{ backgroundColor: '#2D343B', marginBottom: 7, borderRadius: 7 }}>
-            <TouchableOpacity onPress={() => handleClick(bookName)} style={{ padding: 15 }}>
-                <Text style={{ color: 'white', fontSize: 15, fontWeight: 'bold' }}>
-                    {bookName}
-                </Text>
-            </TouchableOpacity>
-            {chapterVisible === bookName && (
-                <View style={{
-                  paddingTop: 15,
-                  paddingLeft: 20,
-                  flexDirection: 'row',
-                  flexWrap: 'wrap',
-                  width: '100%'
-                }}>
-                    {Array(chaptersNumber)
-                      .fill(0)
-                      .map((x, idx) => (
-                            <TouchableOpacity key={idx} style={{
-                              backgroundColor: currentData.book === bookName && currentData.chapter === (idx + 1) ? '#566370' : '#2D343B',
-                              justifyContent: 'center',
-                              alignSelf: 'flex-start',
-                              width: 40,
-                              height: 40,
-                              borderRadius: 20,
-                              marginRight: 15,
-                              marginBottom: 15
-                            }}>
-                                <Text style={{ fontSize: 15, color: 'white', textAlign: 'center', fontWeight: 'bold' }}>
-                                    {idx + 1}
-                                </Text>
-                            </TouchableOpacity>
-                      ))}
-                </View>
-            )}
-        </View>
+      <View style={{ backgroundColor: '#2D343B', marginBottom: 7, borderRadius: 7 }}>
+        <TouchableOpacity onPress={() => handleClick(bookName)} style={{ padding: 15 }}>
+          <Text style={{ color: 'white', fontSize: 15, fontWeight: 'bold' }}>
+            {bookName}
+          </Text>
+        </TouchableOpacity>
+        {chapterVisible === bookName && (
+            <View style={{
+              paddingTop: 15,
+              paddingLeft: 20,
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+              width: '100%'
+            }}>
+              {Array(chaptersNumber)
+                .fill(0)
+                .map((x, idx) => (
+                      <TouchableOpacity key={idx} style={{
+                        backgroundColor: currentData.book === bookName && currentData.chapter === (idx + 1) ? '#566370' : '#2D343B',
+                        justifyContent: 'center',
+                        alignSelf: 'flex-start',
+                        width: 40,
+                        height: 40,
+                        borderRadius: 20,
+                        marginRight: 15,
+                        marginBottom: 15
+                      }}>
+                        <Text style={{ fontSize: 15, color: 'white', textAlign: 'center', fontWeight: 'bold' }}>
+                          {idx + 1}
+                        </Text>
+                      </TouchableOpacity>
+                ))}
+            </View>
+        )}
+      </View>
   )
 
   return (
-        <View style={styles.container}>
-            <View style={styles.centeredView}>
-                {bookData.length > 0 && (
-                    <Modal
-                        animationType="slide"
-                        transparent={true}
-                        visible={modalVisible}
-                        onRequestClose={() => {
-                          setModalVisible(!modalVisible)
-                        }}>
-                        <View style={styles.centeredView}>
-                            <View style={styles.modalView}>
-                                <View style={{ marginTop: 5, marginBottom: 15 }}>
-                                    <Text
-                                        style={{ fontSize: 18, color: 'white', textAlign: 'center', fontWeight: 'bold' }}>
-                                        Nový Zákon
-                                    </Text>
-                                </View>
-                                <FlatList
-                                    data={bibleData.data}
-                                    renderItem={({ item }) => <Item bookName={item.name}/>}
-                                    keyExtractor={item => item.name}
-                                />
-                                <Pressable
-                                    style={[styles.button, styles.buttonClose]}
-                                    onPress={() => setModalVisible(!modalVisible)}>
-                                    <FontAwesomeIcon icon={faXmark} size={18} color={'white'}/>
-                                </Pressable>
-                            </View>
-                        </View>
-                    </Modal>
-                )}
-            </View>
-            <StatusBar barStyle="light-content"/>
+      <View style={styles.container}>
+        <View style={styles.centeredView}>
+          {bookData.length > 0 && (
+              <Modal
+                  animationType="slide"
+                  transparent={true}
+                  visible={modalVisible}
+                  onRequestClose={() => {
+                    setModalVisible(!modalVisible)
+                  }}>
+                <View style={styles.centeredView}>
+                  <View style={styles.modalView}>
+                    <View style={{ marginTop: 5, marginBottom: 15 }}>
+                      <Text
+                          style={{ fontSize: 18, color: 'white', textAlign: 'center', fontWeight: 'bold' }}>
+                        Nový Zákon
+                      </Text>
+                    </View>
+                    <FlatList
+                        data={bibleData.data}
+                        renderItem={({ item }) => <Item bookName={item.name}/>}
+                        keyExtractor={item => item.name}
+                    />
+                    <Pressable
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => setModalVisible(!modalVisible)}>
+                      <FontAwesomeIcon icon={faXmark} size={18} color={'white'}/>
+                    </Pressable>
+                  </View>
+                </View>
+              </Modal>
+          )}
         </View>
+        <StatusBar barStyle="light-content"/>
+      </View>
   )
 }
 
